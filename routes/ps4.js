caec70962fc965d7dd80ec3b643b699107dc2d0e
Johanne Antoine <jantoine@bu.edu>
2021-11-28 23:56:52 -0500

    f

--- routes/ps4.js
+++ routes/ps4.js
@@ -1,90 +1,56 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
-const fetch = require("node-fetch");
+const db = require('../mongoCx');
+
+router.route('/')
+    .get(async (req, res, next) => {  //GET maps to 'read' in REST
+        //connect to db
+        let mongo = await db.getDB('cs591')
+        //do a search, return everything
+        let results = await mongo.collection('peoples').find().toArray(); //returns everything
+        //respond to client
+        res.send(results);
+    })
+
+    // Let's look for a single item in the db, might see a few different ways to pass the params
+    // http://localhost:3000?name=Fred  -- query string, name is on req.params
+    // http://localhost:3000/Fred/MA/BU/  -- param is on URL
+
+    .post(async (req, res, next) => {    //POST maps to 'create' in REST
 
 
-/* GET home page. */
-router.post('/', async function (req, res, next) {
-    const options = {
-        'method': 'POST',
-        'url': 'http://postman-echo.com/post',
-        'headers': {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cookie': 'sails.sid=s%3AjbON0Y6aqJ_ZHMfz3nqrzYcimohvlvRs.72Ahr%2BSxmVU9BE3e3k%2FDOMbWv0x8lQu%2BThHnctbpV7A'
-        },
-        form: {
-            'city': req.body.city
-        }
-    };
-    return new Promise((resolve, reject) => {
-            request(options, function (error, response) {
-                if (error) reject(new Error(error));
-                else {
-                    console.log(response.body);
-                    resolve(response.body);
-                }
-            })
-        }
-    )
+        //connect to db
+        let mongo = await db.getDB('cs591');
+        //insert a record
+        //grab field info from form (method 1):
+        //let userName = req.body.userName;
+        //let results = await mongo.collection('peoples').insert({name: userName, department: 'BUCS'})
 
-        .then(
-            (responseFromBody) => {
-                console.log(`Got a good response: ${responseFromBody}`)
-                res.render('ps4', {response: responseFromBody})
-            },
-            (errorFromReject) => {
-                res.render('ps4', {response: errorFromReject})
-                console.log(`Error: ${errorFromReject}`)
-            }
-        )
+        //grab field info from form (method 2):
+        //just kidding, there is no method 2, just use req.body as long as form field names = db field names
 
+        let results =  await mongo.collection('peoples').insert(req.body)
 
+//        res.send(results);
+        res.send(`Inserted ${req.body.name}`)
+    })
+router.get('/login', (req, res, next) => {
+    //is this route active?  No, order matters (routes are parsed top to bottom)
+    console.log(`Redirecting`);
+    res.redirect('http://localhost:3000/billy')
+//    res.send('logged in')
 
-});
+})
 
-async function ratesAsync() {
-    const fx = await request('http://postman-echo.com/post');
-    return fx;
-}
-
-ratesAsync()
-    .then(function (fx) {
-            console.log(`Rate is ${fx}`) //resolve
-        },
-        (err) => console.log(`${err}`) //reject
-    )
-    .then(() => console.log('All done.'))
-
-const doReq = function (cb) {
-    let options = {
-        method: 'POST',
-        url: 'https://postman-echo.com/post', //reflector
-        qs: {test: '123'},
-        headers: {
-            'postman-token': '9bb9a22f-b509-6c7c-b716-cd4c3106ed0f',
-            'cache-control': 'no-cache'
-        }
-    };
-    console.log('received request');
-
-    request(options, function (error, response, body) {
-        console.log('completed request');
-        const theBody = JSON.parse(body)
-        const test = theBody.args.test;
-        cb(test);
-        if (error) {
-            console.log('oops', error);
-            throw new Error(error); }
-        else {
-            return body;
-        }
-    });
-}
-console.log('starting request')
-let test = doReq((test) => console.log(`${test}`));
-//console.log(test);
-console.log('complete')
+//localhost:3000/Bob
+router.get('/:name', async (req, res, next) => {  //'name' is a bare param, but Express assigns it to req.params.name
+    let mongo = await db.getDB('cs591');
+    //do a search, return matching item
+    let results = await mongo.collection('peoples').findOne({name: req.params.name}, {name: 1, _id: 0}); //returns matching item
+    res.send(`Found ${JSON.stringify(results)}`)
+})
 
 
 module.exports = router;
+
+//flush in-memory (cached) objects to disk
